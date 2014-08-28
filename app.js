2e3cf7d240fcf07e51dc1b64d3746dac0615bb54
mikey <mikey@mikeys-MacBook-Pro.local>
2014-08-27 17:38:20 -0700

    phase 2

--- app.js
+++ app.js
@@ -1,28 +1,60 @@
 $(document).ready(function() {
 
+	/* on keypress run progeam */
+	$('header').on('keypress', 'input', function(event) {
 
-	for (i = 0; i <= 100; i++) {
+		/* on enter key */
+		if(event.which == '13'){
+
+			/* clear any existing fizzbuzz text */
+			$('#container').text( ' ');
+
+			userInput = $('input').val();
+
+			/* if text is a number only, run fizzbuzz */
+			if($.isNumeric(userInput) && +userInput <= 100 && userInput % 1 === 0) {
+
+
+				/*run a loop up the input value that the user entered */
+				for (i = 0; i <= $('input').val(); i++) {
+					
+					if ((i % 3) == 0 && (i % 5) == 0 ){
+						$('#container').append("FizzBuzz " + '<br/>' + '<br/>');
+						console.log("FizzBuzz");
+					}
+					else if (i % 3 == 0) {
+						$('#container').append("Fizz " );
+						console.log("Fizz");
+					}
+					else if (i % 5 == 0) {
+						$('#container').append("Buzz ");
+						console.log("Buzz");
+					}
+
+					else {
+						$('#container').append(i + ' ');
+						console.log(i);
+					}
+				}
+			}
+
+			else if ($.isNumeric(userInput)) {
+				alert("Must be an integer and less than 100!")				
+			}
+
+			/* if text is not a number, alert the user */
+			else {
+				alert("Numbers only please!")
+			}
+
+		}
 		
-		if ((i % 3) == 0 && (i % 5) == 0 ){
-			$('body').append("FizzBuzz ");
-			console.log("FizzBuzz");
-		}
-		else if (i % 3 == 0) {
-			$('body').append("Fizz ");
-			console.log("Fizz");
-		}
-		else if (i % 5 == 0) {
-			$('body').append("Buzz ");
-			console.log("Buzz");
-		}
-
-		else {
-			$('body').append(i + ' ');
-			console.log(i);
-		}
+	})	
 
 
-	}
+
+
+
 
 
 })
